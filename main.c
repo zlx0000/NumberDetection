d029357089959afb9c8faa0e0d5bba538de4ae77
zlx0000 <lxzhang@yandex.com>
2024-10-21 12:31:19 +0100

    modified:   main.c

--- main.c
+++ main.c
@@ -246,7 +246,7 @@ void backprop_l3()
 	y_3 = train_sets_lables[1];
 	for (i = 0; i < 10; i++)
 	{
-		db3[i] = l3[i] * (1 - l3[i]) * (l3[i] - y_3[i]); //Caculating the gradient.
+		db3[i] = l3[i] * (1 - l3[i]) * (l3[i] - y_3[i]); //Caculating the gradient. σ′(x)=σ(x)⋅(1−σ(x))
 	}
 	for (i = 0; i < 10; i++)
 	{
@@ -273,12 +273,18 @@ void backprop_l3()
 void backprop_l2()
 {
 	int i, j;
-	_mm256_store_ps(&y_2[0], dl2p[0]);
-	_mm256_store_ps(&y_2[8], dl2p[1]);
+	//_mm256_store_ps(&y_2[0], dl2p[0]);
+	//_mm256_store_ps(&y_2[8], dl2p[1]);
+	for (i = 0; i < 2; i++)
+	{
+		db2p[i] = _mm256_mul_ps(l2p[i], _mm256_mul_ps(1 - l2p[i], l2p[i] - dl2p[i]));
+	}
+	/*
 	for (i = 0; i < 16; i++)
 	{
 		db2[i] = l2[i] * (1 - l2[i]) * (l2[i] - y_2[i]);
 	}
+	*/
 	for (i = 0; i < 16; i++)
 	{
 		db2p[i] = _mm256_set1_ps(db3[i]);
@@ -297,12 +303,18 @@ void backprop_l2()
 void backprop_l1()
 {
 	int i, j;
-	_mm256_store_ps(&y_1[0], dl1p[0]);
-	_mm256_store_ps(&y_1[8], dl1p[1]);
+	//_mm256_store_ps(&y_1[0], dl1p[0]);
+	//_mm256_store_ps(&y_1[8], dl1p[1]);
+	for (i = 0; i < 2; i++)
+	{
+		db1p[i] = _mm256_mul_ps(l1p[i], _mm256_mul_ps(1 - l1p[i], l1p[i] - dl1p[i]));
+	}
+	/*
 	for (i = 0; i < 16; i++)
 	{
 		db1[i] = l2[i] * (1 - l2[i]) * (l2[i] - y_2[i]);
 	}
+	*/
 	for (i = 0; i < 16; i++)
 	{
 		db1p[i] = _mm256_set1_ps(db3[i]);
